93b5051adfe1f1716cf32097bb8a59d588860c9b
Luka Sever <lukkalukaa@gmail.com>
2022-03-24 13:42:35 +0100

    Made FormView into component instead of view

---
+++ zadaca/src/components/FormComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-form>
+    <v-container>
+        <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="Name"
+            required>
+        </v-text-field>
+        <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate">
+            Submit
+        </v-btn>
+
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name:"FormComponent",
+
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      }
+    }
+</script>
--- zadaca/src/views/ZadacaView.vue
+++ zadaca/src/views/ZadacaView.vue
@@ -1,40 +1,14 @@
 <template>
-  <v-form>
-    <v-container>
-        <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Name"
-            required>
-        </v-text-field>
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate">
-            Submit
-        </v-btn>
-
-    </v-container>
-  </v-form>
+        <form-component />
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-    }),
+import FormComponent from "../components/FormComponent.vue"
+export default {
+    name:"ZadacaView",
 
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      }
+    components: {
+        FormComponent,
     }
+}
 </script>
